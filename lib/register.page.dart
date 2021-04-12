b4bd9bf59a3d05fba9a3786a274577331d2aaed5
jonathan-julio <55097116+jonathan-julio@users.noreply.github.com>
2021-04-12 12:54:20 -0300

    Update register.page.dart

--- lib/register.page.dart
+++ lib/register.page.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-import 'login.page.dart';
+import 'loginPage.dart';
 
 Future<bool> createUser(String email, String password, String name) async {
   var response = await http.post(
